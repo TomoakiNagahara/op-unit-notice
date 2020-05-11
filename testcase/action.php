6e0cac1c11c37248c2f535cb90c7a302b1369511
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-11 18:58:32 +0900

    Mov: testcase > index.php --> action.php

---
+++ testcase/action.php
@@ -0,0 +1,28 @@
+<?php
+/** op-unit-notice:/testcase/sendmail.php
+ *
+ * @creation  2019-04-18
+ * @version   1.0
+ * @package   op-unit-notice
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @creation  2019-04-18
+ */
+namespace OP;
+
+//	...
+Template('menu.phtml');
+
+/* @var array args */
+if( false ){ $args = []; };
+
+//	...
+switch( array_shift($args) ){
+	case 'sendmail':
+		Template('sendmail.php', $args);
+		break;
+}
