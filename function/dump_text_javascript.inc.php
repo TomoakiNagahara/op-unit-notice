bb7333666fa1b4c87eeb5171e95383111a12aeab
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-05-21 17:11:41 +0900

    New: function > dump_text_javascript.inc.php

---
+++ function/dump_text_javascript.inc.php
@@ -0,0 +1,52 @@
+<?php
+/** op-unit-notice:/function/dump_text_javascript.inc.php
+ *
+ * @created   2020-05-21
+ * @version   1.0
+ * @package   op-unit-notice
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\NOTICE;
+
+/** use
+ *
+ */
+use function OP\Decode;
+use function OP\CompressPath;
+
+/* @var $notice array */
+if( false ){
+	$notice = [];
+}
+
+//	...
+if(!$file = $notice['backtrace'][0]['file'] ?? null ){
+	$file = $notice['backtrace'][1]['file'] ?? null;
+};
+
+//	...
+if(!$line = $notice['backtrace'][0]['line'] ?? null ){
+	$line = $notice['backtrace'][1]['line'] ?? null;
+};
+
+//	...
+$file = CompressPath($file);
+
+//	...
+$message = Decode($notice['message']);
+$message = str_replace("'", "\'",$message);
+$message = str_replace('"', '\"',$message);
+$message = str_replace("\n",'\n',$message);
+$message = str_replace("\\",'\\\\',$message);
+
+//	...
+$json = json_encode($notice['backtrace']);
+
+//	...
+echo "console.error('{$file} #{$line} {$message}');".PHP_EOL;
+echo "console.log($json)";
