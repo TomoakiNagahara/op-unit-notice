0e7a7e23853b34a5e0239c683e400422085c933f
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-21 17:14:57 +0900

    Fix: function > dump.php > text/plain --> dump_text_plain.inc.php

--- function/dump.php
+++ function/dump.php
@@ -46,67 +46,6 @@ function Dump( $notice )
 			break;
 
 		default:
-			//	...
-			$count   = $notice['count'];
-			$created = $notice['created'];
-			$message = $notice['message'];
-
-			//	...
-			echo PHP_EOL;
-			echo "{$message} | {$created} (count: $count)".PHP_EOL;
-			echo PHP_EOL;
-
-			//	...
-			foreach( $notice['backtrace'] as $i => $trace ){
-				echo $i . ': ';
-				//	print_r($trace);
-				$join   = [];
-				$file   = $trace['file']     ?? null;
-				$line   = $trace['line']     ?? null;
-				$func   = $trace['function'] ?? null;
-				$type   = $trace['type']     ?? null;
-				$args   = $trace['args']     ??   [];
-				$class  = $trace['class']    ?? null;
-				//	$object = $trace['object']   ?? null;
-
-				//	...
-				if( $file ){
-					echo "{$file} #{$line} ";
-				};
-
-				//	...
-				if( strpos($func, '{closure}') ){
-					echo '{closure}';
-				}else{
-					echo $type ? $class.$type.$func : $func;
-				};
-
-				//	...
-				foreach( $args as $arg ){
-					switch( $type = gettype($arg) ){
-						case 'integer':
-							$join[] = $arg;
-							break;
-
-						case 'string':
-							$join[] = '"'.$arg.'"';
-							break;
-
-						case 'boolean':
-							$join[] = $arg ? 'true': 'false';
-							break;
-
-						case 'array':
-							$join[] = 'array('.count($arg).')';
-							break;
-
-						default:
-							$join[] = $type;
-					};
-				};
-
-				echo '('.join(', ', $join).')'.PHP_EOL;
-			};
-			echo PHP_EOL;
+			require(__DIR__.'/dump_text_plain.inc.php');
 	}
 }
