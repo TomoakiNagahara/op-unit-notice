5120353df55022261dc56a5368b5972dbdc03db7
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-11-14 19:32:19 +0900

    CI: ci.sh

--- ci.sh
+++ ci.sh
@@ -1,11 +1,12 @@
 
  ## op-unit-notice:/ci.sh
  #
+ # Call from git pre-push
+ #
  # @created   2022-11-12
  # @version   1.0
  # @package   op-unit-notice
  # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  # @copyright Tomoaki Nagahara All right reserved.
 
-cd ../../
-php ci.php display=0
+php ../../../ci.php display=0
