5fbb3e6c499c166a1ae517b51a04514a2e8b40b0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-13 15:59:59 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/** op-unit-notice:/config.php
+ *
+ * @created   2023-01-13
+ * @version   1.0
+ * @package   op-unit-notice
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'ci' => true,
+	'cd' => true,
+];
