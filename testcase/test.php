1f93f7eeff373fabdbd0eef69de8bf9ab5886303
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-05-15 14:09:41 +0900

    New: testcase > test.php | Notice test.

---
+++ testcase/test.php
@@ -0,0 +1,17 @@
+<?php
+/** op-unit-notice:/testcase/test.php
+ *
+ * @created     2023-05-11
+ * @version     1.0
+ * @package     op-unit-notice
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//  ...
+Notice::Set('This is notice test.');
